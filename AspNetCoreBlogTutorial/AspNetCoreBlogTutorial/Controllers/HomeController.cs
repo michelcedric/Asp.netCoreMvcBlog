92b10e05d43b053a2fedc337bd4e86881f696343
edn <edn@voxteneo.com>
2017-06-30 14:37:32 +0200

    Add creation mvc site from empty

---
+++ AspNetCoreBlogTutorial/AspNetCoreBlogTutorial/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AspNetCoreBlogTutorial.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AspNetCoreBlogTutorial.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            Person person = new Person()
+            {
+                ID = 1,
+                LastName = "Michel",
+                FirstName = "Cedric"
+            };
+            return View(person);
+        }
+    }
+}
---
+++ AspNetCoreBlogTutorial/AspNetCoreBlogTutorial/Models/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreBlogTutorial.Models
+{
+    public class Person
+    {
+        public int ID { get; set; }
+
+        public string LastName { get; set; }
+
+        public string FirstName { get; set; }
+
+
+    }
+}
